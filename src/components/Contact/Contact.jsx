044eae057e3dac16b8e57fcc855130437f1715e8
Zivgl66 <95753868+Zivgl66@users.noreply.github.com>
2023-12-30 11:16:17 +0200

    contact animations added

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Contact.style.css";
 import icon_star from "../../assets/icons/star.svg";
 import icon_repository from "../../assets/icons/repository.svg";
-import { Fade } from "react-awesome-reveal";
+import { Fade, Zoom } from "react-awesome-reveal";
 
 const Contact = () => {
   return (
@@ -17,31 +17,35 @@ const Contact = () => {
           </div>{" "}
         </Fade>
       </div>
-      <div className="contact_content">
-        <h3>Get In Touch</h3>
-        <p>
-          I'm currently looking for new opportunities and my inbox is always
-          open. Whether you have a job proposition, a question or just want to
-          say hi, I’ll try my best to get back to you!
-        </p>
-        <button className="header_btn">Say hello</button>
-      </div>
-      <div className="contact_footer">
-        <div className="footer_link">
-          <a href="https://github.com/Zivgl66/Portfolio" target="_blank">
-            <p>Desinged & Built by Ziv Gliser</p>
-            <p>
-              <span>
-                <img src={icon_star} alt="star icon" />8
-              </span>{" "}
-              <span>
-                <img src={icon_repository} alt="reposirtory icon" />
-                21
-              </span>
-            </p>
-          </a>
+      <Zoom direction="up" delay={3} triggerOnce={true}>
+        <div className="contact_content">
+          <h3>Get In Touch</h3>
+          <p>
+            I'm currently looking for new opportunities and my inbox is always
+            open. Whether you have a job proposition, a question or just want to
+            say hi, I’ll try my best to get back to you!
+          </p>
+          <button className="header_btn">Say hello</button>
         </div>
-      </div>
+      </Zoom>
+      <Fade direction="up" delay={5} triggerOnce={true}>
+        <div className="contact_footer">
+          <div className="footer_link">
+            <a href="https://github.com/Zivgl66/Portfolio" target="_blank">
+              <p>Desinged & Built by Ziv Gliser</p>
+              <p>
+                <span>
+                  <img src={icon_star} alt="star icon" />8
+                </span>{" "}
+                <span>
+                  <img src={icon_repository} alt="reposirtory icon" />
+                  21
+                </span>
+              </p>
+            </a>
+          </div>
+        </div>
+      </Fade>
     </div>
   );
 };
