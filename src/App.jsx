94dbfe8534191aef58b3bdc8312aaf9113286226
Zivgl66 <95753868+Zivgl66@users.noreply.github.com>
2023-12-30 08:18:55 +0200

    contact component created

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header";
 import About from "./components/About/About";
 import Experience from "./components/Experience/Experience";
 import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <About />
       <Experience />
       <Projects />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Contact.style.css";
+
+const Contact = () => {
+  return (
+    <div id="contact">
+      <div className="margin">
+        <div className="title">
+          <h3>
+            <span className="numbered_header">04. </span>
+            What's next?
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
