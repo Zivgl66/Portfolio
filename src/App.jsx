13da5ea379da4f9d8b20571901c8efdf5a6c9cbf
Zivgl66 <95753868+Zivgl66@users.noreply.github.com>
2024-01-06 10:59:45 +0200

    is mobiel true check

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ function App() {
     <div className="App">
       {typeof navigator !== "undefined" && isMobile() ? (
         ""
+        (setMobile(true))
       ) : (
         <Cursor light={light} />
       )}
