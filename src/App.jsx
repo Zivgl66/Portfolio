f60d9781f78e0ed1461ac38da7804b843e92bd47
Zivgl66 <95753868+Zivgl66@users.noreply.github.com>
2024-01-06 10:57:49 +0200

    false is mobile check

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   return (
     <div className="App">
       {typeof navigator !== "undefined" && isMobile() ? (
-        setMobile((prev) => !prev)
+        ""
       ) : (
         <Cursor light={light} />
       )}
